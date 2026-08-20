0a6a5187b6264893d658441a53cade33db9c95a1
LinetteMi94 <Linakorneva94@mail.ru>
2026-08-20 23:55:15 +1000

    Initial TerraQuest Web API

---
+++ TerraQuest/Controllers/CountriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TerraQuest.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CountriesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCountries()
+    {
+        return Ok("Countries endpoint works!");
+    }
+}
